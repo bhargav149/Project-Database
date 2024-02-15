c106bccf12f4057ca756f7506f08ec893ffccabd
AtinKolli <atin3kolli@gmail.com>
2024-02-15 11:54:39 -0500

    added Dockerfile outside of client and server for CS container

--- init-db.sql
+++ init-db.sql
@@ -8,10 +8,12 @@ CREATE TABLE projects (
     stack TEXT NOT NULL,
     team_name TEXT NOT NULL,
     team_members TEXT NOT NULL,
-    created TIMESTAMP NOT NULL DEFAULT NOW()
-);
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    status ENUM('Completed', 'In-Progress', 'Suspended', 'Unassigned') NOT NULL DEFAULT 'Unassigned');
 
-INSERT INTO projects (title, contents, stack, team_name, team_members)
-VALUES
-('Project Database', 'Create and manage list of projects', 'MySQL, Express.js, React, Node.js', 'Brave Souls', 'Hyunje Kim, Atin Kolli, Prahaara, Bhargav Panchal'),
-('Second project', 'Second project description', 'Second project stack', 'Second project team name', 'Second project team members');
+INSERT INTO projects (title, contents, stack, team_name, team_members, status)
+            VALUES
+            ('Project Database', 'Create and manage list of projects', 'MySQL, Express.js, React, Node.js', 'Brave Souls', 'Hyunje Kim, Atin Kolli, Prahaara, Bhargav Panchal', 'In-Progress'),
+            ('Completed Project', 'Description of completed project...', 'Stack of completed project...', 'Completed Team', 'Completed Team Members', 'Completed'),
+            ('Suspended Project', 'Description of suspended project...', 'Stack of suspended project...', 'Suspended Team', 'Suspended Team Members', 'Suspended'),
+            ('Unassigned Project', 'Description of unassigned project...', 'Stack of unassigned project...', 'Unassigned Team', 'Unassigned Team Members', 'Unassigned');
