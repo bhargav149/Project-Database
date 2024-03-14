05ca9e1c05990b71b17bcdcd2563bbe2ee5a3570
k3h0j8 <hyunjekim2000@gmail.com>
2024-03-14 00:36:29 -0400

    added header, redesign add button and some ui updates

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import AddProjectForm from './components/AddProjectForm';
 import EditProjectModal from './components/EditProjectModal';
 import { FilePenLine } from 'lucide-react';
+import { Plus, X } from 'lucide-react';
 
 function App() {
 
   const [data, setData] = useState([])
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [editingProject, setEditingProject] = useState(null);
+  const [showAddProjectForm, setShowAddProjectForm] = useState(false);
 
   useEffect(() => {
     fetchProjects();
@@ -113,27 +115,48 @@ function App() {
     }
   };
 
+  const toggleAddProjectForm = () => {
+    setShowAddProjectForm(!showAddProjectForm);
+  };
+
   return (
     <div className="container">
-      {data.map((project, i) => (
-        <div key={i} className="card">
-          <p><strong>Title:</strong> {project.title}</p>
-          <p><strong>Description:</strong> {project.contents}</p>
-          <p><strong>Stack:</strong> {project.stack}</p>
-          <p><strong>Team Name:</strong> {project.team_name}</p>
-          <p><strong>Team Members:</strong> {project.team_members}</p>
-          <p><strong>Created:</strong> {project.created}</p>
-          <button className="button-delete" onClick={() => deleteProject(project.id)}>Delete</button>
-          <span className="button-edit" onClick={() => toggleEdit(project)} style={{ cursor: 'pointer' }}>
-            <FilePenLine />
-          </span>
-          <button className={getStatusStyle(project.status)}>{project.status}</button>
+      <header class="site-header">
+        <div className="header-left">
+          <img src={`${process.env.PUBLIC_URL}/vt-logo2.png`} alt="VT Logo" />
+          Brave Souls Project Management System
         </div>
-      ))}
-  
-      <div className="card add-project-card">
-        <AddProjectForm onAdd={addProject} />
+        <div className="login">
+          <button className="login-button">Login</button>
+        </div>
+      </header>
+      <div className="content">
+      <button onClick={toggleAddProjectForm} className="add-project-button">
+        {showAddProjectForm ? <X color="white" size={24} /> : <Plus color="white" size={24} />}
+      </button>
+      <div className="cards-container">
+        {data.map((project, i) => (
+          <div key={i} className="card">
+            <p><strong>Title:</strong> {project.title}</p>
+            <p><strong>Description:</strong> {project.contents}</p>
+            <p><strong>Stack:</strong> {project.stack}</p>
+            <p><strong>Team Name:</strong> {project.team_name}</p>
+            <p><strong>Team Members:</strong> {project.team_members}</p>
+            <p><strong>Created:</strong> {project.created}</p>
+            <button className="button-delete" onClick={() => deleteProject(project.id)}>Delete</button>
+            <span className="button-edit" onClick={() => toggleEdit(project)} style={{ cursor: 'pointer' }}>
+              <FilePenLine />
+            </span>
+            <button className={getStatusStyle(project.status)}>{project.status}</button>
+          </div>
+        ))}
+        </div>
       </div>
+      {showAddProjectForm && (
+        <div className="card add-project-card">
+          <AddProjectForm onAdd={addProject} />
+        </div>
+      )}
 
       {isModalOpen && editingProject && (
         <EditProjectModal
